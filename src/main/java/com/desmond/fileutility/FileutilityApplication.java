3128d52b7afb264ddc938f61a0b82dc01ebf1be1
Desmond <sseekiat@dso.org.sg>
2020-01-16 18:39:06 +0800

    Initial commit

---
+++ src/main/java/com/desmond/fileutility/FileutilityApplication.java
@@ -0,0 +1,13 @@
+package com.desmond.fileutility;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileutilityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileutilityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/desmond/fileutility/FileutilityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desmond.fileutility;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileutilityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
